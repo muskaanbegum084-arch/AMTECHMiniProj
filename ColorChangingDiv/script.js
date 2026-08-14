82375de225a3af7811187997e883b529396d1f9f
muskaanbegum084-arch <ahmedmustaq084@gmail.com>
2026-08-14 16:13:50 +0530

    Changed files

---
+++ ColorChangingDiv/script.js
@@ -0,0 +1,10 @@
+let div=document.querySelector("div");
+let btn=document.querySelector("button");
+let h2=document.querySelector("h2");
+btn.addEventListener("click",()=>{
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    div.style.backgroundColor=`rgb(${red},${green},${blue})`;
+    h2.innerText=`rgb(${red},${green},${blue})`
+})
